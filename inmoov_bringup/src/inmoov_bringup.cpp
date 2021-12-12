0dcfea509871a63ec8bd835988a62fcef84fb11e
crystal-net <gckemper@gmail.com>
2021-12-12 15:07:53 -0800

    add broken changes

--- inmoov_bringup/src/inmoov_bringup.cpp
+++ inmoov_bringup/src/inmoov_bringup.cpp
@@ -1,9 +1,9 @@
-#include "inmoov_bringup/inmoov_bringup.hpp"
+// #include "inmoov_bringup/inmoov_bringup.hpp"
 #include "rclcpp/rclcpp.hpp"
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<rclcpp::Node>("my_node_name");
+    auto node = std::make_shared<rclcpp::Node>("inmoov_bringup");
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
