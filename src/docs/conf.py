39bca469e74228fee889c59042a9dd749c626928
crystal-net <gckemper@gmail.com>
2022-05-01 17:50:03 -0700

    add logo

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'inmoov_ros2'
+project = 'InMoov on ROS2'
 copyright = '2021, Crystal Networks'
 author = 'Crystal Networks'
 
@@ -31,6 +31,9 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx_multiversion",
+    "sphinx_rtd_theme",
+    "sphinx_copybutton",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,3 +56,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = "_static/logo.png"
