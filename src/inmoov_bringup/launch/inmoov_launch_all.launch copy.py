6441f899e6f3e7020e2d1f17e0a57bb79885ffb2
crystal-net <gckemper@gmail.com>
2022-07-09 12:05:02 -0700

    update launcher

--- src/inmoov_bringup/launch/inmoov_launch_all.launch copy.py
+++ src/inmoov_bringup/launch/inmoov_launch_all.launch copy.py
@@ -5,6 +5,8 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+import xacro
 
 
 def generate_launch_description():
@@ -58,36 +60,34 @@ def generate_launch_description():
 
 
 
-# =========================
+# # =========================
 
-    # Use xacro to process the file
-    xacro_file = os.path.join(get_package_share_directory(pkg_name),file_subpath)
-    robot_description_raw = xacro.process_file(xacro_file).toxml()
-
-
-    # Configure the node
-    node_robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[{'robot_description': robot_description_raw}] # add other parameters here if required
-    )
+#     # Use xacro to process the file
+#     xacro_file = os.path.join(get_package_share_directory(pkg_name),file_subpath)
+#     robot_description_raw = xacro.process_file(xacro_file).toxml()
 
 
-    # node_rviz2 = Node(package='rviz2', node_executable='rviz2', output='screen')
+#     # Configure the node
+#     node_robot_state_publisher = Node(
+#         package='robot_state_publisher',
+#         executable='robot_state_publisher',
+#         output='screen',
+#         parameters=[{'robot_description': robot_description_raw}] # add other parameters here if required
+#     )
 
 
-    # Run the node
-    return LaunchDescription([
-        node_robot_state_publisher
-    ])
-
-    # Run the rviz2
-    # return LaunchDescription([
-    #     node_rviz2
-    # ])
+#     # node_rviz2 = Node(package='rviz2', node_executable='rviz2', output='screen')
 
 
+#     # Run the node
+#     return LaunchDescription([
+#         node_robot_state_publisher
+#     ])
 
-    from launch_ros.actions import Node
-import xacro
+#     # Run the rviz2
+#     # return LaunchDescription([
+#     #     node_rviz2
+#     # ])
+
+
+
