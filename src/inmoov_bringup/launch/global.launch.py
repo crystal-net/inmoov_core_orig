6b12e2671a4ab7d4303aa6b2079f5799feb6418b
crystal-net <gckemper@gmail.com>
2022-07-18 20:06:39 -0700

    LAUNCHER UPDATE

--- src/inmoov_bringup/launch/global.launch.py
+++ src/inmoov_bringup/launch/global.launch.py
@@ -7,11 +7,15 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    config = os.path.join(
-      get_package_share_directory('inmoov_bringup'),
-      'config',
-      'inmoov_config.yaml'
-    )
+
+
+    # Build configuration variables
+    inmoov_config = os.path.join(get_package_share_directory('inmoov_bringup'),'config','inmoov_config.yaml')
+    rviz_config = os.path.join(get_package_share_directory('inmoov_bringup'),'config','conf.rviz')
+    xacro_config = os.path.join(get_package_share_directory('inmoov_description'),'description','example_robot.urdf.xacro')
+
+
+
 
     return LaunchDescription([
         Node(
@@ -19,15 +23,38 @@ def generate_launch_description():
             executable='talker',
             namespace='inmoov', # Namespace
             name='sim',                # Subnodename
-            parameters=[config]
+            parameters=[inmoov_config]
         ),
         Node(
             package='micro_ros_agent',
             executable='micro_ros_agent',
             name='micro_ros_agent',
             arguments=["serial", "--dev", "/dev/ttyACM0"]
+        ),
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher'
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config]
+        )
+    ])
 
-            # namespace='inmoov', # Namespace
-            # name='uros',                # Subnodename
-            # parameters=[{'serial --dev /dev/ttyACM0'}]
-        )])
+
+        #     Node(
+        #     package='robot_state_publisher',
+        #     executable='robot_state_publisher',
+        #     parameters=[{'use_sim_time': use_sim_time, 
+        #     'robot_description': Command(['xacro ', urdf_model])}],
+        #     arguments=[default_urdf_model_path]
+        # ),
